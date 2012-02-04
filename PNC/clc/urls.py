f7a325c5125c1248d9ae72776858bb18415f1335
Peer Xu <pppeerxu@gmail.com>
2012-02-04 23:38:44 +0800

    modified:   PNC/PNC/settings.py 	new file:   PNC/clc/templates/404.html 	modified:   PNC/clc/templates/base.html 	modified:   PNC/clc/templates/nav.html 	modified:   PNC/clc/templates/user.html 	new file:   PNC/clc/templates/user/login.html 	new file:   PNC/clc/templates/user/register.html 	modified:   PNC/clc/urls.py 	modified:   PNC/clc/views.py 	new file:   PNC/database/database.db

--- PNC/clc/urls.py
+++ PNC/clc/urls.py
@@ -12,6 +12,8 @@ urlpatterns = patterns('',
      {'document_root': FILE_PATH + '/images'}),
     (r'^js/(?P<path>.*)$', 'django.views.static.serve',
      {'document_root': FILE_PATH + '/js'}),
+    ('^register$', "clc.views.view_register"),
+    ('^login$', "clc.views.view_login"),
     ('^demo/hello', "clc.views.view_hello"),
     ('^demo/base', TemplateView.as_view(template_name='base.html')),
 )
--- PNC/clc/views.py
+++ PNC/clc/views.py
@@ -1,5 +1,35 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 
 def view_hello(request):
     return render_to_response('hello_world.html')
-    
+
+def view_register(request):
+    if request.method == "POST":
+#        import pdb; pdb.set_trace()
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect("/clc/demo/base")
+    else:
+        form = UserCreationForm()
+    c = RequestContext(request, {
+            'form': form
+    })
+    return render_to_response("user/register.html",context_instance=c)
+
+def view_login(request):
+    if request.method == "POST":
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        import pdb; pdb.set_trace()
+        user = auth.authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect("/clc/demo/base")
+    return render_to_response("user/login.html",context_instance=RequestContext(request, {}))
