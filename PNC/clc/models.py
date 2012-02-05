388952631fbabda330ab8b29993b7467ac9f6b33
Peer Xu <pppeerxu@gmail.com>
2012-02-05 22:56:35 +0800

    modified:   PNC/clc/models.py 	modified:   PNC/clc/templates/logo.html 	modified:   PNC/clc/templates/user/login.html 	modified:   PNC/clc/urls.py 	modified:   PNC/database/database.db

--- PNC/clc/models.py
+++ PNC/clc/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+#class PathMapping(models.Model):
+#    template_name = models.CharField(max_length=255)
+#    current_path = models.CharField(max_length=255)
+#    parent_mapping = models.ForeignKey(PathMapping, blank=True, null=True)
+#    full_path = models.CharField(max_length=255, blank=True, null=True)
+#    rebuild_flag = models.BooleanField() # not done
